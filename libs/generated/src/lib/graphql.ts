c96191be8ca7c9e215eb047a7373a584174c39b4
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-12-26 17:10:18 +0550

    feat: generated new graphql queries

--- libs/generated/src/lib/graphql.ts
+++ libs/generated/src/lib/graphql.ts
@@ -21,16 +21,148 @@ export type Scalars = {
   Float: number;
 };
 
+/** The standard interface that contains the error message when something goes wrong */
+export type ApiError = {
+  /** The error message giving details about what went wrong */
+  message: Scalars['String'];
+};
+
+/** Type returned for the error when a new user is created. */
+export type CreateUserError = {
+  __typename: 'CreateUserError';
+  /** General errors relating to the registration attempt */
+  message?: Maybe<Scalars['String']>;
+  /** Whether a user with this username already exists */
+  userExists: Scalars['Boolean'];
+};
+
+/** Result type returned as the result when new user is created. */
+export type CreateUserResultUnion = CreateUserError | UserDto;
+
+/** The type returned for the errors when login is unsuccessful */
+export type LoginError = ApiError & {
+  __typename: 'LoginError';
+  /** The error message corresponding to this login attempt */
+  message: Scalars['String'];
+};
+
+/** The type returned on successful login */
+export type LoginResult = {
+  __typename: 'LoginResult';
+  /** Wether the login attempt was successful */
+  status: Scalars['Boolean'];
+  /** The user this login result is associated with */
+  user: UserDto;
+};
+
+/** Result type returned as the result when someone tries to login */
+export type LoginResultUnion = LoginError | LoginResult;
+
+export type Mutation = {
+  __typename: 'Mutation';
+  /** Mutation to create a new user with a given authentication token. */
+  createUser: CreateUserResultUnion;
+  /** Login using an authentication token. */
+  loginUser: LoginResultUnion;
+};
+
+export type MutationCreateUserArgs = {
+  DIDToken: Scalars['String'];
+};
+
+export type MutationLoginUserArgs = {
+  DIDToken: Scalars['String'];
+};
+
 export type Query = {
   __typename: 'Query';
   /** Get status of the service. */
   getStatus: Scalars['Boolean'];
 };
 
+/** Critical details about a user of the service */
+export type UserDto = {
+  __typename: 'UserDto';
+  /** The primary key of the user */
+  id: Scalars['ID'];
+};
+
+export type LoginUserMutationVariables = Exact<{
+  DIDToken: Scalars['String'];
+}>;
+
+export type LoginUserMutation = {
+  __typename: 'Mutation';
+  loginUser:
+    | { __typename: 'LoginError'; message: string }
+    | {
+        __typename: 'LoginResult';
+        status: boolean;
+        user: { __typename: 'UserDto'; id: string };
+      };
+};
+
+export type CreateUserMutationVariables = Exact<{
+  DIDToken: Scalars['String'];
+}>;
+
+export type CreateUserMutation = {
+  __typename: 'Mutation';
+  createUser:
+    | {
+        __typename: 'CreateUserError';
+        userExists: boolean;
+        message?: string | null | undefined;
+      }
+    | { __typename: 'UserDto'; id: string };
+};
+
 export type GetStatusQueryVariables = Exact<{ [key: string]: never }>;
 
 export type GetStatusQuery = { __typename: 'Query'; getStatus: boolean };
 
+export const LoginUserDocument = gql`
+  mutation LoginUser($DIDToken: String!) {
+    loginUser(DIDToken: $DIDToken) {
+      __typename
+      ... on LoginResult {
+        status
+        user {
+          id
+        }
+      }
+      ... on LoginError {
+        message
+      }
+    }
+  }
+`;
+
+export function useLoginUserMutation() {
+  return Urql.useMutation<LoginUserMutation, LoginUserMutationVariables>(
+    LoginUserDocument
+  );
+}
+export const CreateUserDocument = gql`
+  mutation CreateUser($DIDToken: String!) {
+    createUser(DIDToken: $DIDToken) {
+      __typename
+      ... on UserDto {
+        id
+      }
+      ... on CreateUserError {
+        userExists
+        message
+      }
+    }
+  }
+`;
+
+export function useCreateUserMutation() {
+  return Urql.useMutation<CreateUserMutation, CreateUserMutationVariables>(
+    CreateUserDocument
+  );
+}
 export const GetStatusDocument = gql`
   query GetStatus {
     getStatus
