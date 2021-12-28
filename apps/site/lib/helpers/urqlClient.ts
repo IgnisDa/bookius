f59f0630768d40d40f62c5901df637699d9ea52a
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-12-28 17:23:16 +0550

    feat(urqlClient.ts): attach auth header to request

--- apps/site/lib/helpers/urqlClient.ts
+++ apps/site/lib/helpers/urqlClient.ts
@@ -1,3 +1,4 @@
+import Cookies from 'js-cookie';
 import {
   createClient,
   ssrExchange,
@@ -5,13 +6,19 @@ import {
   cacheExchange,
   fetchExchange,
 } from 'urql';
+import { AUTH_TOKEN_KEY } from '../constants';
 
 const isServerSide = typeof window === 'undefined';
+
 export const ssrCache = ssrExchange({ isClient: !isServerSide });
+
 export const client = createClient({
   url: process.env.NEXT_PUBLIC_GRAPHQL_API,
   exchanges: [dedupExchange, cacheExchange, ssrCache, fetchExchange],
-  fetchOptions: () => {
-    return { headers: {} };
-  },
+  fetchOptions: () =>
+    isServerSide
+      ? {}
+      : {
+          headers: { authorization: `Bearer ${Cookies.get(AUTH_TOKEN_KEY)}` },
+        },
 });
