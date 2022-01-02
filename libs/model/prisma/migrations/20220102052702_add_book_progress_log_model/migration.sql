8f1aac5f07186ae377cf8cc2f33fb0b68163f830
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-01-02 12:56:47 +0550

    feat(migration.sql, schema.prisma): add book progress log model

---
+++ libs/model/prisma/migrations/20220102052702_add_book_progress_log_model/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "BookProgressLog" (
+    "id" BIGSERIAL NOT NULL,
+    "percentage" DECIMAL(5, 2) NOT NULL DEFAULT 0 CHECK (
+        "percentage" >= 0
+        AND "percentage" <= 100
+    ),
+    "bookId" BIGINT NOT NULL,
+    "numPages" SMALLINT NOT NULL CHECK ("percentage" > 0),
+    "userId" TEXT NOT NULL,
+    "startedOn" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedOn" TIMESTAMP(3) NOT NULL,
+    CONSTRAINT "BookProgressLog_pkey" PRIMARY KEY ("id")
+);
+-- CreateIndex
+CREATE UNIQUE INDEX "BookProgressLog_bookId_userId_key" ON "BookProgressLog"("bookId", "userId");
+-- AddForeignKey
+ALTER TABLE "BookProgressLog"
+ADD CONSTRAINT "BookProgressLog_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "Book"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+-- AddForeignKey
+ALTER TABLE "BookProgressLog"
+ADD CONSTRAINT "BookProgressLog_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
