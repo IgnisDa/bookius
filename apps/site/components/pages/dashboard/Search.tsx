2281000a9a0d3671aa2b2a9f2d1a6ebd927f10e8
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-01-09 10:20:53 +0000

    fix: layout issues on theme change

--- apps/site/components/pages/dashboard/Search.tsx
+++ apps/site/components/pages/dashboard/Search.tsx
@@ -1,13 +1,13 @@
 export const SearchComponent = () => {
   return (
     <div className="space-y-4">
-      <h1 className="font-bold text-700 dark:text-2xl font-heading dark:text-accent">
+      <h1 className="text-2xl font-bold text-700 font-heading text-base-100 dark:text-accent">
         Find your favorite books/authors
       </h1>
       <fieldset className="flex w-full space-x-2">
         <input
           type="text"
-          className="flex-1 input input-ghost input-bordered dark:text-black"
+          className="flex-1 text-gray-900 input input-ghost input-bordered dark:text-gray-500 focus:bg-gray-200 focus:dark:bg-gray-800 focus:text-gray-900 focus:dark:text-gray-200"
           placeholder="Enter something and press enter"
         />
         <button className="btn btn-secondary dark:btn-primary">Go</button>
