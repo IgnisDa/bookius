613135422efb0a45abdd04503bb6595247395c3a
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-01-10 14:05:45 +0000

    feat: extract error pages to dedicated component

---
+++ apps/site/components/pages/ErrorContainer.tsx
@@ -0,0 +1,52 @@
+import NextImage from 'next/image';
+import Link from 'next/link';
+import { FC, ReactElement } from 'react';
+
+type ErrorComponentPageProps = {
+  errorCode: number;
+  mainText: string;
+  linkText: string;
+  image: StaticImageData;
+};
+
+export const getErrorLayout = (page: ReactElement) => (
+  <main className="flex flex-col items-center justify-center w-screen h-screen bg-cream-white dark:bg-base-300">
+    {page}
+  </main>
+);
+
+export const ErrorComponentPage: FC<ErrorComponentPageProps> = ({
+  errorCode,
+  mainText,
+  linkText,
+  image,
+}) => {
+  return (
+    <div className="flex flex-col items-center justify-center p-5 space-y-10 lg:space-x-reverse lg:space-x-10 lg:flex-row-reverse lg:space-y-0">
+      <div className="space-y-4 text-center lg:space-y-2">
+        <h1 className="text-6xl font-semibold tracking-tighter text-rose-500 dark:text-red-600">
+          {errorCode}
+        </h1>
+        <h2 className="text-4xl text-base-300 dark:text-blue-300">
+          {mainText}
+        </h2>
+        <div>
+          <Link href="/dashboard">
+            <a className="text-gray-600 underline hover:text-blue-600 dark:text-base-content dark:hover:text-warning">
+              {linkText}
+            </a>
+          </Link>
+        </div>
+      </div>
+      <div>
+        <NextImage
+          src={image}
+          height={'500px'}
+          width={'500px'}
+          layout="intrinsic"
+          priority
+        />
+      </div>
+    </div>
+  );
+};
--- apps/site/pages/500.tsx
+++ apps/site/pages/500.tsx
@@ -1,43 +1,20 @@
-import NextImage from 'next/image';
-import Link from 'next/link';
-import { ReactElement } from 'react';
+import {
+  ErrorComponentPage,
+  getErrorLayout,
+} from '../components/pages/ErrorContainer';
 import Image500 from '../public/images/500-image.png';
 
 const ServerErrorPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center p-5 space-y-10 lg:space-x-reverse lg:space-x-10 lg:flex-row-reverse lg:space-y-0">
-      <div className="space-y-4 text-center lg:space-y-2">
-        <h1 className="text-6xl font-semibold tracking-tighter text-rose-500 dark:text-red-600">
-          500
-        </h1>
-        <h2 className="text-4xl text-base-300 dark:text-blue-300">
-          You just broke something...
-        </h2>
-        <div>
-          <Link href="/dashboard">
-            <a className="text-gray-600 underline hover:text-blue-600 dark:text-base-content dark:hover:text-warning">
-              Take me back, it wasn't me, I swear!
-            </a>
-          </Link>
-        </div>
-      </div>
-      <div>
-        <NextImage
-          src={Image500}
-          height={'600px'}
-          width={'600px'}
-          layout="intrinsic"
-          priority
-        />
-      </div>
-    </div>
+    <ErrorComponentPage
+      errorCode={500}
+      mainText="You just broke something..."
+      linkText="Take me back, it wasn't me, I swear!"
+      image={Image500}
+    />
   );
 };
 
-ServerErrorPage.getLayout = (page: ReactElement) => (
-  <main className="flex flex-col items-center justify-center w-screen h-screen bg-cream-white dark:bg-base-300">
-    {page}
-  </main>
-);
+ServerErrorPage.getLayout = getErrorLayout;
 
 export default ServerErrorPage;
