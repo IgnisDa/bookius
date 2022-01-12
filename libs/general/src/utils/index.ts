5af1d9cbd53ee55b1ff32a227a99ecb5d5148224
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-01-12 09:22:59 +0000

    feat(server): integrate with open library API
    Also remove Google API settings

--- libs/general/src/utils/index.ts
+++ libs/general/src/utils/index.ts
@@ -1,6 +1,7 @@
 import { ValidationError } from '@nestjs/common';
 import { plainToClass } from 'class-transformer';
 import { validate } from 'class-validator';
+import { isObject, isArray, reduce, camelCase } from 'lodash';
 
 export async function validatePlainObjectByDecorators(
   obj: any,
@@ -15,3 +16,24 @@ export async function validatePlainObjectByDecorators(
   });
   return errors;
 }
+
+// I have absolutely no idea how this thing works. Apparently its a recursive function or
+// something. I will just leave the SO link:
+// https://stackoverflow.com/questions/12931828/convert-returned-json-object-properties-to-lower-first-camelcase
+export const camelCaseKeys: any = (obj: any) => {
+  if (!isObject(obj)) {
+    return obj;
+  } else if (isArray(obj)) {
+    return obj.map((v) => camelCaseKeys(v));
+  }
+  return reduce(
+    obj,
+    (r, v, k) => {
+      return {
+        ...r,
+        [camelCase(k)]: camelCaseKeys(v),
+      };
+    },
+    {}
+  );
+};
