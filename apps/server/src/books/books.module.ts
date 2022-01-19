a72ece6944fb2fd0292c9219a35a5be478a7d4a7
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-01-19 04:43:55 +0000

    feat: add queue processor for books module

--- apps/server/src/books/books.module.ts
+++ apps/server/src/books/books.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksResolver } from './books.resolver';
+import { BookProcessor } from './books.processor';
+import { BullModule } from '@nestjs/bull';
+import { MODULE_QUEUE_NAME } from './books.constants';
 
 @Module({
-  providers: [BooksResolver, BooksService],
+  imports: [
+    BullModule.registerQueue({
+      name: MODULE_QUEUE_NAME,
+    }),
+  ],
+  providers: [BooksResolver, BooksService, BookProcessor],
 })
 export class BooksModule {}
---
+++ apps/server/src/books/books.processor.ts
@@ -0,0 +1,14 @@
+import { UpdateJobLogsProcessor, logger } from '@bookius/general';
+import { Process, Processor } from '@nestjs/bull';
+import { Job } from 'bull';
+import { COLLECT_BOOKS_DATA_JOB, MODULE_QUEUE_NAME } from './books.constants';
+
+@Processor(MODULE_QUEUE_NAME)
+export class BookProcessor extends UpdateJobLogsProcessor {
+  @Process(COLLECT_BOOKS_DATA_JOB)
+  async collectBookData(job: Job<{ possibleIsbn: string[] }>) {
+    const { possibleIsbn } = job.data;
+    logger.info(possibleIsbn);
+    return;
+  }
+}
