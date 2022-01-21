d4d73e957e9b5092f79455c22cfba57870509bdb
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-01-21 11:35:01 +0000

    feat: adapt to changed graphql schema

--- apps/site/components/pages/book/DetailsDisplay.tsx
+++ apps/site/components/pages/book/DetailsDisplay.tsx
@@ -1,15 +1,17 @@
-import { OpenLibraryWorkDetailsDto } from '@bookius/generated';
+import { BookDetailsFragment } from '@bookius/generated';
 import Image from 'next/image';
 import { FC } from 'react';
 import noBookImage from '../../../public/images/no-book.png';
 
 type DetailsDisplayComponentProps = {
-  book: OpenLibraryWorkDetailsDto;
+  book: BookDetailsFragment;
 };
 
 export const DetailsDisplayComponent: FC<DetailsDisplayComponentProps> = ({
   book,
 }) => {
+  book;
+  const hasCovers = book.bookImages.length > 0;
   const isbn = [];
   if (book.isbn13) isbn.push(...book.isbn13);
   if (book.isbn10) isbn.push(...book.isbn10);
@@ -18,15 +20,13 @@ export const DetailsDisplayComponent: FC<DetailsDisplayComponentProps> = ({
     <>
       <div className="relative h-[500px] w-[320px] sm:h-[550px] sm:w-[370px] flex-none lg:h-[700px] lg:w-[450px]">
         <Image
-          src={book.covers?.at(0) || noBookImage}
-          placeholder={!!book.covers ? 'blur' : 'empty'}
+          src={hasCovers ? book.bookImages?.at(0)?.coverUrl! : noBookImage}
+          placeholder={hasCovers ? 'blur' : 'empty'}
           blurDataURL={
-            !!book.blurImageBase64Strings
-              ? book.blurImageBase64Strings.at(0)
-              : undefined
+            hasCovers ? book.bookImages.at(0)?.base64String : undefined
           }
           alt={
-            !!book.covers
+            hasCovers
               ? `Image for '${book.title}'`
               : 'No valid book covers found'
           }
@@ -39,45 +39,15 @@ export const DetailsDisplayComponent: FC<DetailsDisplayComponentProps> = ({
         <h1 className="text-4xl font-bold text-left md:text-5xl font-heading text-secondary-content">
           {book.title}
         </h1>
-        {book.authors && (
+        {book.architects && (
           <h2 className="text-gray-100 ">
             <span className="opacity-75 md:text-lg">by</span>{' '}
             <span className="text-xl underline md:text-2xl">
-              {book.authors.map((a) => a.name).join(', ')}
+              {book.architects.map((a) => a.author.name).join(', ')}
             </span>
           </h2>
         )}
         <div className="h-8" />
-        {book.numberOfPages && (
-          <p>
-            <span className="text-sm text-gray-500 md:text-base lg:text-lg">
-              Pages:
-            </span>{' '}
-            <span className="font-semibold text-gray-300 md:text-xl lg:text-2xl">
-              {book.numberOfPages}
-            </span>
-          </p>
-        )}
-        {book.publishDate && (
-          <p>
-            <span className="text-sm text-gray-500 md:text-base lg:text-lg">
-              Published on:
-            </span>{' '}
-            <span className="font-semibold text-gray-300 md:text-xl lg:text-2xl">
-              {book.publishDate}
-            </span>
-          </p>
-        )}
-        {book.publishers && (
-          <p>
-            <span className="text-sm text-gray-500 md:text-base lg:text-lg">
-              Published by:
-            </span>{' '}
-            <span className="font-semibold text-gray-300 md:text-xl lg:text-2xl">
-              {book.publishers.join(', ')}
-            </span>
-          </p>
-        )}
         {isbn.length > 0 && (
           <p>
             <span className="text-sm text-gray-500 md:text-base lg:text-lg">
--- apps/site/pages/shelves/index.tsx
+++ apps/site/pages/shelves/index.tsx
@@ -1,26 +1,17 @@
-import { useGetUserShelvesShortQuery } from '@bookius/generated';
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
-import { GET_USER_SHELVES_SHORT } from '../../graphql/queries';
 import {
-  getFetchOptions,
   getRedirectUnauthenticatedRequests,
   hasRequiredRequestCookies,
 } from '../../lib/helpers/getAuthOptions';
-import { client, ssrCache } from '../../lib/helpers/urqlClient';
 
 const ShelvesIndex = (
   _props: InferGetServerSidePropsType<typeof getServerSideProps>
 ) => {
-  const [{ data }, executeGetUserShelvesShortQuery] =
-    useGetUserShelvesShortQuery();
-
-  const refreshShelves = async () =>
-    executeGetUserShelvesShortQuery({ requestPolicy: 'network-only' });
-
   return (
     <>
-      {/* <CreateShelfButton refreshShelves={refreshShelves} /> */}
-      <div>{/* <pre>{JSON.stringify(data, null, 4)}</pre> */}</div>
+      <div>
+        <h1>All shelves</h1>
+      </div>
     </>
   );
 };
@@ -31,10 +22,7 @@ export const getServerSideProps = async ({
   if (!hasRequiredRequestCookies(req))
     return getRedirectUnauthenticatedRequests();
 
-  await client
-    .query(GET_USER_SHELVES_SHORT, undefined, getFetchOptions(req))
-    .toPromise();
-  return { props: { urqlState: ssrCache.extractData() } };
+  return { props: {} };
 };
 
 export default ShelvesIndex;
