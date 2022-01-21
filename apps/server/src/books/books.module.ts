52ef0781b8d0a0381ca2bf1000534b4f971dd793
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-01-21 09:58:00 +0000

    feat: register open library collector service

--- apps/server/src/books/books.module.ts
+++ apps/server/src/books/books.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
-import { BooksService } from './books.service';
-import { BooksResolver } from './books.resolver';
-import { BookProcessor } from './books.processor';
+import { OpenLibraryCollector } from '@bookius/data';
 import { BullModule } from '@nestjs/bull';
+import { Module } from '@nestjs/common';
 import { MODULE_QUEUE_NAME } from './books.constants';
+import { BookProcessor } from './books.processor';
+import { BooksResolver } from './books.resolver';
+import { BooksService } from './books.service';
 
 @Module({
   imports: [
@@ -11,6 +12,6 @@ import { MODULE_QUEUE_NAME } from './books.constants';
       name: MODULE_QUEUE_NAME,
     }),
   ],
-  providers: [BooksResolver, BooksService, BookProcessor],
+  providers: [BooksResolver, BooksService, BookProcessor, OpenLibraryCollector],
 })
 export class BooksModule {}
