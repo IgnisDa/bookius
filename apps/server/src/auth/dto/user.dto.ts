d75134961b317a734bca2e72ec722fc138106fb9
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-01-21 09:55:04 +0000

    feat: remove usage og generated schema

--- apps/server/src/auth/dto/user.dto.ts
+++ apps/server/src/auth/dto/user.dto.ts
@@ -1,7 +1,11 @@
-import { User as GeneratedUser } from '@bookius/generated/prisma-nestjs-graphql';
-import { ObjectType, PickType } from '@nestjs/graphql';
+import { ObjectType } from '@nestjs/graphql';
 
 @ObjectType({
   description: 'Critical details about a user of the service',
 })
-export class UserDto extends PickType(GeneratedUser, ['id'] as const) {}
+export class UserDto {
+  /**
+   * The unique identifier for this user
+   */
+  id: string;
+}
