882e00260abb37df6335d250015960c8b3750dfa
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-01-21 09:14:47 +0000

    feat: change data param names and types

--- apps/server/src/books/books.processor.ts
+++ apps/server/src/books/books.processor.ts
@@ -6,9 +6,9 @@ import { COLLECT_BOOKS_DATA_JOB, MODULE_QUEUE_NAME } from './books.constants';
 @Processor(MODULE_QUEUE_NAME)
 export class BookProcessor extends UpdateJobLogsProcessor {
   @Process(COLLECT_BOOKS_DATA_JOB)
-  async collectBookData(job: Job<{ possibleIsbn: string[] }>) {
-    const { possibleIsbn } = job.data;
-    logger.info(possibleIsbn);
+  async collectBookData(job: Job<{ isbn: string[] }>) {
+    const { isbn } = job.data;
+    logger.info(isbn);
     return;
   }
 }
