584cadbf2046431bbb9c4a4de5d326cd10de0fc1
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-02-02 15:18:13 +0000

    feat: add animation on stats page

--- apps/site/components/pages/book/StatDisplay.tsx
+++ apps/site/components/pages/book/StatDisplay.tsx
@@ -1,4 +1,27 @@
-import { FC } from 'react';
+import { animate } from 'framer-motion';
+import { createRef, FC, useEffect } from 'react';
+
+type StatsCounterComponentProps = {
+  from: number;
+  to: number;
+};
+
+const StatsCounterComponent: FC<StatsCounterComponentProps> = ({
+  from,
+  to,
+}) => {
+  const nodeRef = createRef<HTMLParagraphElement>();
+  useEffect(() => {
+    const node = nodeRef.current!;
+    const controls = animate(from, to, {
+      duration: 2,
+      onUpdate: (value) => (node.textContent = value.toFixed(0)),
+    });
+    return () => controls.stop();
+  }, [from, to]);
+
+  return <span ref={nodeRef} />;
+};
 
 type StatDisplayComponentProps = {
   prefix: string;
@@ -13,11 +36,11 @@ export const StatDisplayComponent: FC<StatDisplayComponentProps> = ({
 }) => {
   return (
     <div>
-      <span className="block text-lg md:text-2xl">{prefix}</span>
-      <span className="mt-2 block text-4xl font-semibold text-accent sm:text-5xl md:text-6xl">
-        {value}
-      </span>
-      <span className="block text-lg md:text-2xl">{suffix}</span>
+      <p className="block text-lg md:text-2xl">{prefix}</p>
+      <p className="mt-2 block text-4xl font-semibold text-accent sm:text-5xl md:text-6xl">
+        <StatsCounterComponent from={0} to={value} />
+      </p>
+      <p className="block text-lg md:text-2xl">{suffix}</p>
     </div>
   );
 };
