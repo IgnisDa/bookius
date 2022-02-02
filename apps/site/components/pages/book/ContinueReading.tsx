015e6ae00ec9591c9e31e25d31bd3b231bf2702f
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-02-02 03:54:06 +0000

    fix(book-details): bug in production mode

--- apps/site/components/pages/book/ContinueReading.tsx
+++ apps/site/components/pages/book/ContinueReading.tsx
@@ -6,7 +6,7 @@ import {
   useStartReadingBookMutation,
   useUpdateBookReadingProgressMutation,
 } from '@bookius/generated';
-import * as Slider from '@radix-ui/react-slider';
+import Slider from '@mui/material/Slider';
 import { AnimatePresence, motion } from 'framer-motion';
 import debounce from 'lodash.debounce';
 import { FC, useEffect, useMemo, useState } from 'react';
@@ -74,24 +74,24 @@ export const ContinueReadingComponent: FC<ContinueReadingComponentProps> = ({
 
   return (
     <div className="w-full space-y-4 sm:space-y-6 md:space-y-10">
-      <div className="flex justify-around flex-1 w-full text-center">
+      <div className="flex w-full flex-1 justify-around text-center">
         <div>
           <span className="block text-lg md:text-2xl">You read</span>
-          <span className="block mt-2 text-lg font-semibold md:text-6xl text-accent">
+          <span className="mt-2 block text-lg font-semibold text-accent md:text-6xl">
             {completedBooks}
           </span>
           <span className="block text-lg md:text-2xl">times</span>
         </div>
         <div>
           <span className="block text-lg md:text-2xl">Read by</span>
-          <span className="block mt-2 text-lg font-semibold md:text-6xl text-accent">
+          <span className="mt-2 block text-lg font-semibold text-accent md:text-6xl">
             {getBookStatisticsQueryData?.bookStatistics.readBy || 0}
           </span>
           <span className="block text-lg md:text-2xl">others</span>
         </div>
         <div>
           <span className="block text-lg md:text-2xl">Reviewed by</span>
-          <span className="block mt-2 text-lg font-semibold md:text-6xl text-accent">
+          <span className="mt-2 block text-lg font-semibold text-accent md:text-6xl">
             {getBookStatisticsQueryData?.bookStatistics.reviewedBy || 0}
           </span>
           <span className="block text-lg md:text-2xl">readers</span>
@@ -105,7 +105,7 @@ export const ContinueReadingComponent: FC<ContinueReadingComponentProps> = ({
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
-                className="px-6 py-2 mx-auto text-3xl font-normal capitalize transition-transform rounded-lg btn btn-primary hover:scale-105"
+                className="btn btn-primary mx-auto rounded-lg px-6 py-2 text-3xl font-normal capitalize transition-transform hover:scale-105"
                 onClick={async () => {
                   await executesStartReadingBookMutation({
                     input: { bookId: book.id, numPages: 300 },
@@ -120,30 +120,21 @@ export const ContinueReadingComponent: FC<ContinueReadingComponentProps> = ({
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
-                className="flex items-center space-x-4"
+                className="flex w-60 select-none items-center space-x-4 md:w-72 lg:w-80 xl:w-96 2xl:w-[450px]"
               >
-                <Slider.Root
-                  defaultValue={[latestBookProgressLog.percentage]}
-                  onValueChange={(value) => {
-                    const progress = value.at(0)!;
+                <Slider
+                  aria-label="Always visible"
+                  defaultValue={latestBookProgressLog.percentage}
+                  onChange={(_, value) => {
+                    const progress = value as number;
                     setReadingProgress(progress);
                     handleUpdateReadingProgress(
                       progress,
                       latestBookProgressLog.id
                     );
                   }}
-                  max={100}
-                  min={0}
-                  step={1}
-                  aria-label="progress"
-                  className="relative flex items-center h-5 select-none w-60 md:w-72 lg:w-80 xl:w-96 2xl:w-[450px]"
-                >
-                  <Slider.Track className="relative flex-1 h-1.5 bg-purple-300 rounded-full">
-                    <Slider.Range className="absolute block h-full bg-purple-700 rounded-full" />
-                  </Slider.Track>
-                  <Slider.Thumb className="block w-5 h-5 bg-white border-gray-600 rounded-full focus:border-2" />
-                </Slider.Root>
-                <span className="px-2 pt-1 rounded-md md:text-xl bg-base-200">
+                />
+                <span className="rounded-md bg-base-200 px-2 pt-1 md:text-xl">
                   {readingProgress}%
                 </span>
               </motion.div>
@@ -151,8 +142,8 @@ export const ContinueReadingComponent: FC<ContinueReadingComponentProps> = ({
           </AnimatePresence>
         </div>
       ) : (
-        <div className="flex items-center justify-center w-full">
-          <LoadingIcon className="w-8 h-8 ease-in-out animate-spin" />
+        <div className="flex w-full items-center justify-center">
+          <LoadingIcon className="h-8 w-8 animate-spin ease-in-out" />
         </div>
       )}
     </div>
